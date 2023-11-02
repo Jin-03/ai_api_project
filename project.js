69ab9e16c375e2af82c31b64610dcb159bc09f9c
Jin-03 <101050716+Jin-03@users.noreply.github.com>
2023-11-02 12:01:12 +0900

    project file

---
+++ project.js
@@ -0,0 +1,84 @@
+var OPENAI_API_KEY = "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";
+
+let word1
+let word2
+let words
+
+function Send(){
+  var sQuestion = txtMsg.value + " 연관 단어를 번호를 매겨서 출력해줘";
+  var data = {
+        model: "text-davinci-003",
+        prompt: sQuestion,
+        max_tokens: 100,
+        temperature: 0
+  }
+  $.ajax({
+    type: "POST",
+    url: 'https://api.openai.com/v1/completions',
+    headers:{
+        "Accept" : "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + OPENAI_API_KEY },
+        data: JSON.stringify(data),
+
+  }).done(function(response) {
+    
+    var sanswer = response.choices[0].text;
+    word1 = sanswer.substring(
+      sanswer.search(1) + 3,
+      sanswer.search(2) - 1
+    );
+    word2 = sanswer.substring(
+      sanswer.search(2) + 3,
+      sanswer.search(3) - 1
+    );
+
+    words = [word1, word2];
+    txtOut.value = sanswer;
+    // txtOut.value = words;
+    Draw(words);
+
+  }).fail(function(error) {
+    alert("!/js/user.js에서 에러발생: " + error.statusText);
+    console.log(error);
+  });
+}
+
+function Draw(words){
+  for (let i = 0; i < 2; i++) {
+    var sQuestion = words[i];
+    console.log(words[i]);
+
+    var data = {
+          prompt: sQuestion,
+          n:1,
+          size: "256x256"
+    }
+
+    $.ajax({
+      type: "POST",
+      url: 'https://api.openai.com/v1/images/generations',
+      headers:{
+          "Accept" : "application/json",
+          "Content-Type": "application/json", 
+          "Authorization": "Bearer " + OPENAI_API_KEY },
+      data: JSON.stringify(data),
+
+    }).done(function(response) {
+      const container = document.querySelector('.imgContainer');
+
+      const img = new Image()
+
+      
+      img.src = response.data[0].url
+      img.alt = words[i]
+      container.appendChild(img)
+
+    }).fail(function(error) {
+      alert("!/js/user.js에서 에러발생: " + error.statusText);
+      console.log(error)
+
+    });
+  }
+  
+}
---
+++ test.js
@@ -0,0 +1,200 @@
+var OPENAI_API_KEY = "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";
+
+let words = [];
+let prompts = [];
+
+function Send(){
+  let sQuestion = "Generate related words with " + txtMsg.value + " and print them out numbered.";
+  console.log(sQuestion)
+  let data = {
+        model: "text-davinci-003",
+        prompt: sQuestion,
+        max_tokens: 100,
+        temperature: 0
+  }
+  $.ajax({
+    type: "POST",
+    url: 'https://api.openai.com/v1/completions',
+    headers:{
+        "Accept" : "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + OPENAI_API_KEY },
+        data: JSON.stringify(data),
+      
+  }).done(function(response) {
+    let word0 = txtMsg.value
+    let sanswer = response.choices[0].text;
+    let word1 = sanswer.substring(
+      sanswer.search(1) + 2,
+      sanswer.search(2) - 1
+    );
+    let word2 = sanswer.substring(
+      sanswer.search(2) + 2,
+      sanswer.search(3) - 1
+    );
+    let word3 = sanswer.substring(
+      sanswer.search(3) + 2,
+      sanswer.search(4) - 1
+    );
+    let word4 = sanswer.substring(
+      sanswer.search(4) + 2,
+      sanswer.search(5) - 1
+    );
+
+    words = [word0, word1, word2, word3, word4];
+    txtOut.value = sanswer;
+    console.log(sanswer);
+    txtOut2.value = words;
+    console.log(words);
+      MakePrompt(words, 4);
+      MakePrompt(words, 3);
+      MakePrompt(words, 2);
+      MakePrompt(words, 1);
+      MakePrompt(words, 0);
+
+  }).fail(function(error) {
+    alert("!/js/user.js에서 에러발생: " + error.statusText);
+    console.log(error);
+  });
+}
+
+
+// function MakePrompt(words){
+//   for (let i = 0; i < 5; i++) {
+//     let sQuestion = "Generate a prompt for drawing " + words[i] + " in english";
+//     console.log(sQuestion)
+//     let data = {
+//           model: "text-davinci-003",
+//           prompt: sQuestion,
+//           max_tokens: 100,
+//           temperature: 0
+//     }
+//     $.ajax({
+//       type: "POST",
+//       url: 'https://api.openai.com/v1/completions',
+//       headers:{
+//           "Accept" : "application/json",
+//           "Content-Type": "application/json",
+//           "Authorization": "Bearer " + OPENAI_API_KEY },
+//           data: JSON.stringify(data),
+
+//     }).done(function(response) {
+      
+//       let sanswer = response.choices[0].text;
+
+//       console.log(sanswer)
+//       prompts[i] = sanswer;
+      
+//       // Draw(words);
+
+//     }).fail(function(error) {
+//       alert("!/js/user.js에서 에러발생: " + error.statusText);
+//       console.log(error);
+//     });
+
+//   }
+
+//   console.log(prompts)
+//   txtOut3.value = prompts;
+
+// }
+
+function MakePrompt(words, i) {
+
+  let sQuestion = "Generate a prompt for drawing " + words[i] + " in english";
+  console.log(sQuestion)
+  let data = {
+        model: "text-davinci-003",
+        prompt: sQuestion,
+        max_tokens: 100,
+        temperature: 0
+  }
+  $.ajax({
+    type: "POST",
+    url: 'https://api.openai.com/v1/completions',
+    headers:{
+        "Accept" : "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + OPENAI_API_KEY },
+        data: JSON.stringify(data),
+
+  }).done(function(response) {
+    
+    let sanswer = response.choices[0].text;
+    console.log(sanswer);
+    prompts[i] = sanswer;
+    console.log(prompts);
+    txtOut3.value = prompts;
+    
+    // Draw(words);
+
+  }).fail(function(error) {
+    alert("!/js/user.js에서 에러발생: " + error.statusText);
+    console.log(error);
+  });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Draw(words){
+  for (let i = 0; i < 5; i++) {
+    
+    let sQuestion = words[i];
+    console.log(sQuestion);
+
+    let data = {
+      prompt: sQuestion,
+      n:1,
+      size: "256x256"
+    }
+
+    $.ajax({
+      type: "POST",
+      url: 'https://api.openai.com/v1/images/generations',
+      headers:{
+          "Accept" : "application/json",
+          "Content-Type": "application/json", 
+          "Authorization": "Bearer " + OPENAI_API_KEY },
+      data: JSON.stringify(data),
+
+    }).done(function(response) {
+      const container = document.querySelector('.imgContainer');
+
+      const img = new Image()
+
+      
+      img.src = response.data[0].url
+      img.alt = words[i]
+      container.appendChild(img)
+
+    }).fail(function(error) {
+      alert("!/js/user.js에서 에러발생: " + error.statusText);
+      console.log(error)
+
+    });
+
+  }
+  
+}
