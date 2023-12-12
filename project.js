00a32d000233ff5b60a7e0b085b4c2ce2514a820
Jin-03 <101050716+Jin-03@users.noreply.github.com>
2023-12-12 12:32:12 +0900

    Add files via upload

--- project.js
+++ project.js
@@ -1,12 +1,64 @@
-var OPENAI_API_KEY = "";
+const OPENAI_API_KEY = "";
 
-let word1
-let word2
-let words
+let words = [];
+let prompts = [];
 
 function Send(){
-  var sQuestion = txtMsg.value + " 연관 단어를 번호를 매겨서 출력해줘";
-  var data = {
+  let sQuestion = "Generate related words with " + txtMsg.value + " and print them out numbered.";
+  console.log(sQuestion)
+  let data = {
+        model: "text-davinci-003",
+        prompt: sQuestion,
+        max_tokens: 100,
+        temperature: 0
+  }
+  $.ajax({
+    type: "POST",
+    url: 'https://api.openai.com/v1/completions',
+    headers:{
+        "Accept" : "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + OPENAI_API_KEY },
+    data: JSON.stringify(data),
+      
+  }).done(function(response) {
+    let word0 = txtMsg.value
+    let sanswer = response.choices[0].text;
+    let word1 = sanswer.substring(
+      sanswer.search(1) + 2,
+      sanswer.search(2) - 1
+    );
+    let word2 = sanswer.substring(
+      sanswer.search(2) + 2,
+      sanswer.search(3) - 1
+    );
+    let word3 = sanswer.substring(
+      sanswer.search(3) + 2,
+      sanswer.search(4) - 1
+    );
+
+    words = [word0, word1, word2, word3];
+    // txtOut.value = sanswer;
+    console.log(sanswer);
+    txtOut2.value = words;
+    console.log(words);
+      MakePrompt(words, 3);
+      MakePrompt(words, 2);
+      MakePrompt(words, 1);
+      MakePrompt(words, 0);
+
+  }).fail(function(error) {
+    alert("!/js/user.js에서 에러발생: " + error.statusText);
+    console.log(error);
+  });
+}
+
+
+function MakePrompt(words, i) {
+
+  let sQuestion = "Generate a prompt for drawing " + words[i] + " in english";
+  console.log(sQuestion)
+  let data = {
         model: "text-davinci-003",
         prompt: sQuestion,
         max_tokens: 100,
@@ -23,20 +75,12 @@ function Send(){
 
   }).done(function(response) {
     
-    var sanswer = response.choices[0].text;
-    word1 = sanswer.substring(
-      sanswer.search(1) + 3,
-      sanswer.search(2) - 1
-    );
-    word2 = sanswer.substring(
-      sanswer.search(2) + 3,
-      sanswer.search(3) - 1
-    );
-
-    words = [word1, word2];
-    txtOut.value = sanswer;
-    // txtOut.value = words;
-    Draw(words);
+    let sanswer = response.choices[0].text;
+    console.log(sanswer);
+    prompts[i] = sanswer;
+    console.log(prompts);
+    // txtOut3.value = prompts;
+  
 
   }).fail(function(error) {
     alert("!/js/user.js에서 에러발생: " + error.statusText);
@@ -44,15 +88,17 @@ function Send(){
   });
 }
 
-function Draw(words){
-  for (let i = 0; i < 2; i++) {
-    var sQuestion = words[i];
-    console.log(words[i]);
 
-    var data = {
-          prompt: sQuestion,
-          n:1,
-          size: "256x256"
+function Draw(words){
+  for (let i = 0; i < 4; i++) {
+    
+    let sQuestion = words[i];
+    console.log(sQuestion);
+
+    let data = {
+      prompt: sQuestion,
+      n:1,
+      size: "256x256"
     }
 
     $.ajax({
@@ -79,6 +125,7 @@ function Draw(words){
       console.log(error)
 
     });
+
   }
   
 }
